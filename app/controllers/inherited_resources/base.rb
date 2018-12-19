ae3070149062f92a55e24a75198e82d68c0ce80b
Raido Paaslepp <raido.paaslepp@eesti.ee>
2018-12-19 13:48:20 +0200

    Make InheritedResources::Base parent controller configurable

--- app/controllers/inherited_resources/base.rb
+++ app/controllers/inherited_resources/base.rb
@@ -8,7 +8,10 @@ module InheritedResources
   # call <tt>default</tt> class method, call <<tt>actions</tt> class method
   # or overwrite some helpers in the base_helpers.rb file.
   #
-  class Base < ::ApplicationController
+  # By default the base class inherits from ::ApplicationController
+  # This can be changed through an initializer:
+  #   InheritedResources.parent_controller = "Admin::ApplicationController"
+  class Base < InheritedResources.parent_controller
     # Overwrite inherit_resources to add specific InheritedResources behavior.
     def self.inherit_resources(base)
       base.class_eval do
--- lib/inherited_resources.rb
+++ lib/inherited_resources.rb
@@ -23,6 +23,15 @@ module InheritedResources
   def self.flash_keys=(array)
     Responders::FlashResponder.flash_keys = array
   end
+
+  def self.parent_controller=(controller_klass)
+    controller_klass = controller_klass.constantize if controller_klass.is_a?(String)
+    @parent_controller = controller_klass
+  end
+
+  def self.parent_controller
+    @parent_controller ||= ::ApplicationController
+  end
 end
 
 ActiveSupport.on_load(:action_controller) do
